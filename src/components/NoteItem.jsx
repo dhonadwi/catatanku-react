b96010d6756129095214ca95d9ac5d8ba39d26c8
dhonadwi <dhonadwi2@gmail.com>
2024-12-12 09:51:38 +0700

    fix formatedDate

--- src/components/NoteItem.jsx
+++ src/components/NoteItem.jsx
@@ -1,12 +1,13 @@
 import React from 'react';
 import Button from './Button';
+import showFormattedDate from '../utils';
 
 export default function NoteItem({ note, onArchive, onDelete }) {
   return (
     <div className="note-item">
       <div className="note-item__content">
         <h3 className="note-item__title">{note.title}</h3>
-        <p className="note-item__date">{note.createdAt}</p>
+        <p className="note-item__date">{showFormattedDate(note.createdAt)}</p>
         <p className="note-item__body">{note.body}</p>
       </div>
       <Button onClick={() => onArchive(note.id)} text={'Pindahkan'} />
